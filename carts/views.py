2662693280a51c6bbae442bb0958313fdb548bf1
Igor Dos Santos <a2sigor@gmail.com>
2023-05-31 18:44:49 +0100

    Added Search Functionality

--- carts/views.py
+++ carts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from store.models import Product
 from .models import Cart, CartItem
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
 from django.http import HttpResponse
@@ -56,6 +57,8 @@ def remove_cart_item(request, product_id):
 
 def cart(request, total=0, quantity=0, cart_items=None):
     try:
+        tax = 0
+        grand_total = 0
         cart = Cart.objects.get(cart_id=_cart_id(request))
         cart_items = CartItem.objects.filter(cart=cart, is_active=True)
         for cart_item in cart_items:
@@ -63,7 +66,7 @@ def cart(request, total=0, quantity=0, cart_items=None):
             quantity += cart_item.quantity
         tax = (2 * total)/100
         grand_total = total + tax
-    except ObjectNotExist:
+    except ObjectDoesNotExist:
         pass #just ignore
 
     context = {
--- store/views.py
+++ store/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, get_object_or_404
-
-from carts.models import CartItem
 from .models import Product
 from category.models import Category
+from carts.models import CartItem
+from django.db.models import Q
+
+
 
 from carts.views import _cart_id
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.http import HttpResponse
 # Create your views here.
 
 def store(request, category_slug=None):
@@ -14,13 +18,19 @@ def store(request, category_slug=None):
     if category_slug != None:
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=categories, is_available=True)
+        paginator = Paginator(products, 1) #here is where you can set the number of items to be displayed on the page
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
+        product_count = products.count()
     else:
-        products = Product.objects.filter(is_available=True)
-    
-    product_count = products.count()
+        products = Product.objects.all().filter(is_available=True).order_by('id')
+        paginator = Paginator(products, 3) #here is where you can set the number of items to be displayed on the page
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
+        product_count = products.count()
 
     context = {
-        'products': products,
+        'products': paged_products,
         'product_count': product_count, 
     }
     return render(request, 'store/store.html', context)
@@ -38,3 +48,17 @@ def product_detail(request, category_slug, product_slug):
         'in_cart'       : in_cart,
     }
     return render(request, 'store/product_detail.html', context)
+
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products = Product.objects.order_by('-created_date').filter(Q(description__icontains=keyword) | Q(product_name__icontains=keyword)) #this will loop through the list of products searching for anything related to the keyword
+            product_count = products.count()
+    context = {
+        'products' : products,
+        'product_count' : product_count,
+    }
+    return render(request, 'store/store.html', context)
+
+
