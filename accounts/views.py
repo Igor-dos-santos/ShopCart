49a29395e4a9d6eb68487b66413f30a21495fda1
Igor Dos Santos <a2sigor@gmail.com>
2023-06-22 14:50:34 +0100

    User registration set up

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm 
+from accounts.models import Account
+from django.contrib import messages
 
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            phone_number = form.cleaned_data['phone_number']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            username = email.split("@")[0]
+            user = Account.object.create_user(first_name=first_name, last_name=last_name, email=email, username=username, password=password)
+            user.phone_number = phone_number
+            user.save()
+            messages.success(request, 'Registration Successful.')
+            return redirect('register')
+
+    else:
+        form = RegistrationForm()
+    context =  {
+        'form' : form,
+    }
+    return render(request, 'accounts/register.html', context)
+
+def login(request):
+    return render(request, 'accounts/login.html')
+
+def logout(request):
+    return
