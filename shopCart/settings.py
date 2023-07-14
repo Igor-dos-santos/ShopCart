612fb582d524b7484ffa97e94e899df9b4a276b4
Igor Dos Santos <a2sigor@gmail.com>
2023-07-14 15:13:34 +0100

    Security Measures Applied

--- shopCart/settings.py
+++ shopCart/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 't4*je!))31u#^g!yo36*(&)i*lv=mk9ja&8xj^gjzap-m_)onn'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool) #True
 
 ALLOWED_HOSTS = []
 
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
     'store',
     'carts',
     'orders',
+    'admin_honeypot',
 ]
 
 MIDDLEWARE = [
@@ -52,7 +54,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_session_timeout.middleware.SessionTimeoutMiddleware',
 ]
+
+SESSION_EXPIRE_SECONDS = 3600 # the session will automatically expire after 1 hour of inactivity
+SESSION_EXPIRE_AFTER_LAST_ACTIVITY = True
+SESSION_TIMEOUT_REDIRECT = 'accounts/login'
 
 ROOT_URLCONF = 'shopCart.urls'
 
@@ -142,18 +149,10 @@ MESSAGE_TAGS = {
     messages.ERROR: "danger",
 }
 
-
-#SMTP configuration
-#EMAIL_HOST = 'smtp.gmail.com'
-#EMAIL_PORT = 587 #Gmail code for the port
-#EMAIL_HOST_USER = 'igor.work02@gmail.com'
-#EMAIL_HOST_PASSWORD = ''
-#EMAIL_USE_TLS = True
-# SMTP configuration for Outlook.com (Hotmail)
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp-mail.outlook.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'shop.cart@outlook.com'
-EMAIL_HOST_PASSWORD = 'ShopCart!Email23!'
-EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = 'shop.cart@outlook.com'
+EMAIL_BACKEND = config('EMAIL_BACKEND')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
